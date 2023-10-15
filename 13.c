52aa1c0d30f1b65ca3ba307bb636ca90256d0cca
JEONGYONGKANG <147419909+JEONGYONGKANG@users.noreply.github.com>
2023-10-15 22:33:58 +0900

    Add files via upload

---
+++ 13.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void) {
+	int n;
+	int result = 0;
+	scanf("%d", &n);
+
+	for (int i = 1; i <=n; i++) {
+		result = result + i;
+	}
+	printf("%d", result);
+	return 0;
+}
