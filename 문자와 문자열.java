3e16f26ba3f5106e519bfd2f7b05dd6b70938c3b
JEONGYONGKANG <147419909+JEONGYONGKANG@users.noreply.github.com>
2023-11-14 13:41:52 +0900

    문자와 문자열.java

---
+++ 문자와 문자열.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class b27866 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s= sc.nextLine();
+		int x = sc.nextInt();
+		
+		String str = new String(s);
+		
+		System.out.println(str.charAt(x-1));
+		
+
+	}
+
+}
